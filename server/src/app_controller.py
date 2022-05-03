e414ba261f608c1737e00cd3bea5f50f7d38f036
Jesús Alejandro Yahuitl Rodríguez <jesusaleyaro@hotmail.com>
2022-05-03 00:29:21 -0500

    refactor: set method name as health check

--- server/src/app_controller.py
+++ server/src/app_controller.py
@@ -3,5 +3,5 @@ from flask import Blueprint
 app_controller = Blueprint('app_controller', __name__)
 
 @app_controller.route("/")
-def hello():
+def health_check():
     return "Server is working. ✨"
