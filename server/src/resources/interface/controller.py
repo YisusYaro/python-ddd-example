99b7f58a46d756865e0ac2aab58cfdbfd26e25a3
Jesús Alejandro Yahuitl Rodríguez <jesusaleyaro@hotmail.com>
2022-05-05 02:24:29 -0500

    feat(resources): use command bus

--- server/src/resources/interface/controller.py
+++ server/src/resources/interface/controller.py
@@ -2,9 +2,10 @@ from flask import Blueprint, Response, request
 
 from ..application.commands.create_resource_command import \
     CreateResourceCommand
+from shared.infraestructure.dependency_injection.app_container import AppContainer
 
 resources_controller = Blueprint('resources_controller', __name__)
-
+command_bus = AppContainer().container.CommandBus()
 
 @resources_controller.route("/resources", methods=['GET'])
 def get_resource():
@@ -15,4 +16,5 @@ def get_resource():
 def create_resource():
     body = request.get_json()
     command = CreateResourceCommand(id=body["id"], name=body["name"])
+    command_bus.execute(command)
     return Response(status=204)
