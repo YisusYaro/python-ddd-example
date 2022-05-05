406ec86d93777ab6cbaf94a169565879103974d0
Jesús Alejandro Yahuitl Rodríguez <jesusaleyaro@hotmail.com>
2022-05-05 02:25:34 -0500

    feat(resources): inject repository

--- server/src/resources/application/commands/create_resource_handler.py
+++ server/src/resources/application/commands/create_resource_handler.py
@@ -1,6 +1,15 @@
-class GetClientProfileHandler(object):
-    def __init__(self):
-        pass
+from importlib.resources import Resource
+from dependency_injector.wiring import Provide, inject
+from resources.infraestructure.repositories.resource_respository import ResourceRepository
+from shared.infraestructure.dependency_injection.app_container import AppContainer
+from dependency_injector.wiring import Provide, inject
+
+
+class CreateResourceHandler(object):
+    @inject
+    def __init__(self, resource_repository: ResourceRepository = AppContainer().container.ResourceRepository()):
+        self.resource_repository = resource_repository
 
     def handle(self, command):
-        print(command)
+        self.resource_repository.save(command.name)
+        print("handling", command.name)
