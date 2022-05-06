c063d43c7cfa2965d730ccf6fdad5d193011fd99
Jesús Alejandro Yahuitl Rodríguez <jesusaleyaro@hotmail.com>
2022-05-06 01:33:40 -0500

    feat: add aggregate root

---
+++ server/src/shared/domain/aggregate_root.py
@@ -0,0 +1,14 @@
+from shared.infraestructure.dependency_injection import app
+
+
+class AggregateRoot(object):
+    def __init__(self):
+        self.__events = []
+        self.__eventBus = app.App().container.EventBus()
+
+    def record(self, event):
+        self.__events.append(event)
+
+    def commit(self):
+        for event in self.__events:
+            self.__eventBus.execute(event)
