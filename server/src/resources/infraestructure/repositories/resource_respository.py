a9a02ad386c08ccd2b4a3d9e76251098ce992235
Jesús Alejandro Yahuitl Rodríguez <jesusaleyaro@hotmail.com>
2022-05-06 00:32:40 -0500

    feat(resources): add find by id repository method

--- server/src/resources/infraestructure/repositories/resource_respository.py
+++ server/src/resources/infraestructure/repositories/resource_respository.py
@@ -1,9 +1,21 @@
 from shared.infraestructure.repositories.mongo_repository import MongoRepository
+from resources.domain.factory import Factory
 
 
 class ResourceRepository(MongoRepository):
     def __init__(self):
-        super().__init__('resources')
+        super().__init__("resources")
 
     def save(self, resource):
         self.persist(resource)
+
+    def find_by_id(self, id):
+        document = self.collection().find_one({"_id": id})
+
+        if(document is None):
+            return None
+
+        return self.document_to_resource(document)
+
+    def document_to_resource(self, document):
+        return Factory.reconstitute(id=document["_id"], name=document["name"], status=document["status"])
