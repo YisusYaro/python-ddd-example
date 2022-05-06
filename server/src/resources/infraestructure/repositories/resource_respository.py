7a8839443c2bd42bbd238b20a91a904ceb3364fb
Jesús Alejandro Yahuitl Rodríguez <jesusaleyaro@hotmail.com>
2022-05-05 19:52:21 -0500

    feat(resources): add repository

--- server/src/resources/infraestructure/repositories/resource_respository.py
+++ server/src/resources/infraestructure/repositories/resource_respository.py
@@ -1,3 +1,9 @@
-class ResourceRepository(object):
+from shared.infraestructure.repositories.mongo_repository import MongoRepository
+
+
+class ResourceRepository(MongoRepository):
+    def __init__(self):
+        super().__init__('resources')
+
     def save(self, resource):
-        print('saving', resource)
+        self.persist(resource)
