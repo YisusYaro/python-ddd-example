2a962d6f5827cde9e7670563646b485311ede597
Jesús Alejandro Yahuitl Rodríguez <jesusaleyaro@hotmail.com>
2022-05-06 01:35:24 -0500

    refactor(resources): separate module elements

--- server/src/resources/infraestructure/dependency_injection/resources_module.py
+++ server/src/resources/infraestructure/dependency_injection/resources_module.py
@@ -5,17 +5,39 @@ from shared.infraestructure.dependency_injection.app import App
 from resources.application.commands.create_resource_handler import CreateResourceHandler
 from resources.application.queries.get_resource_handler import GetResourceHandler
 from resources.infraestructure.repositories.resource_respository import ResourceRepository
+from resources.application.events.resource_created_handler import ResourceCreatedHandler
 
 
-def setResourcesModule():
+def setCommands():
     App().container.CreateResourceCommand = providers.Factory(
         CreateResourceHandler,
     )
 
+
+def setQueries():
     App().container.GetResourceQuery = providers.Factory(
         GetResourceHandler,
     )
 
+
+def setEvents():
+    App().container.ResourceCreatedEvent = providers.Factory(
+        ResourceCreatedHandler,
+    )
+
+
+def setApplication():
+    setCommands()
+    setQueries()
+    setEvents()
+
+
+def setInfraestructure():
     App().container.ResourceRepository = providers.Singleton(
         ResourceRepository,
     )
+
+
+def setResourcesModule():
+    setApplication()
+    setInfraestructure()
